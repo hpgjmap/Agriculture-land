ae4185433361e378c3ef716e1193551bc70d42d3
hpgjmap <hpgjmapsolutions@gmail.com>
2025-04-17 09:43:10 +0530

    Update CaptureImage.jsx

--- src/components/CaptureImage.jsx
+++ src/components/CaptureImage.jsx
@@ -173,7 +173,7 @@ const CaptureImage = ({ setCameraActive, view }) => {
       style={{
         position: "relative",
         width: "100vw",
-        height: "100vh",
+        height: "100dvh",
         overflow: "hidden",
         backgroundColor: "black",
       }}
